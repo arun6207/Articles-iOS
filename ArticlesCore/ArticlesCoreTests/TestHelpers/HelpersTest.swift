c1a36c9dd061bbc6fba02a5ada7d763eace8d02b
Arun Amuri <arunkumar6207@gmail.com>
2021-02-20 10:55:24 +0400

    added protocol stub

---
+++ ArticlesCore/ArticlesCoreTests/TestHelpers/HelpersTest.swift
@@ -0,0 +1,31 @@
+//
+//  HelpersTest.swift
+//  ArticlesCoreTests
+//
+//  Created by Arun Amuri on 20/02/2021.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
+
+func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+    let json = ["items": items]
+    return try! JSONSerialization.data(withJSONObject: json)
+}
+
+extension HTTPURLResponse {
+    convenience init(statusCode: Int) {
+        self.init(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+    }
+}

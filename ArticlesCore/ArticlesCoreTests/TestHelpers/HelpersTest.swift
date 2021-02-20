e7d9bcb369cf6783e3b0a37b535ead117b784fd4
Arun Amuri <arunkumar6207@gmail.com>
2021-02-20 11:02:55 +0400

    add test cases for url sessions http client

--- ArticlesCore/ArticlesCoreTests/TestHelpers/HelpersTest.swift
+++ ArticlesCore/ArticlesCoreTests/TestHelpers/HelpersTest.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 func anyNSError() -> NSError {
-    return NSError(domain: "any error", code: 0)
+    return NSError(domain: "error", code: 0)
 }
 
 func anyURL() -> URL {
-    return URL(string: "http://any-url.com")!
+    return URL(string: "http://url.com")!
 }
 
 func anyData() -> Data {
-    return Data("any data".utf8)
+    return Data("sample data".utf8)
 }
 
-func makeItemsJSON(_ items: [[String: Any]]) -> Data {
-    let json = ["items": items]
-    return try! JSONSerialization.data(withJSONObject: json)
-}
+//func makeItemsJSON(_ items: [[String: Any]]) -> Data {
+//    let json = ["items": items]
+//    return try! JSONSerialization.data(withJSONObject: json)
+//}
 
 extension HTTPURLResponse {
     convenience init(statusCode: Int) {
